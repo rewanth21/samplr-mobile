43cfbafb86274132711a529ac67f25f42519bf7b
Manikandan Nagarajan <manikandan.n2010@gmail.com>
2016-03-22 13:27:07 -0400

    setup-1

--- src/constants/Service.js
+++ src/constants/Service.js
@@ -1,1 +1,1 @@
-export const DEV_API_ROOT = 'http://ec2-54-186-220-145.us-west-2.compute.amazonaws.com/';
+export const DEV_API_ROOT = 'http://ec2-54-186-220-145.us-west-2.compute.amazonaws.com:3000/';
