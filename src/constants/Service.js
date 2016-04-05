4b644ef2df539bffe27ee7a5e1b08c437aaec051
Manikandan Nagarajan <manikandan.n2010@gmail.com>
2016-04-05 18:30:39 -0400

    API integration

--- src/constants/Service.js
+++ src/constants/Service.js
@@ -1,1 +1,1 @@
-export const DEV_API_ROOT = 'http://ec2-54-191-34-182.us-west-2.compute.amazonaws.com:3000/';
+export const DEV_API_ROOT = 'http://ec2-54-187-87-240.us-west-2.compute.amazonaws.com:3000/';
