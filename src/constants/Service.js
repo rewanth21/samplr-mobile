244a52697816e0753f8acf97f8180cc7f8526ea4
Manikandan Nagarajan <manikandan.n2010@gmail.com>
2016-04-11 15:36:12 -0400

    API integ

--- src/constants/Service.js
+++ src/constants/Service.js
@@ -1,1 +1,1 @@
-export const DEV_API_ROOT = 'http://ec2-54-200-192-172.us-west-2.compute.amazonaws.com:3000/';
+export const DEV_API_ROOT = 'http://ec2-54-218-38-134.us-west-2.compute.amazonaws.com:3000/';
