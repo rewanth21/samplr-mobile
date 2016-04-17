cb81fa68d7d22f2788d2986994fb482cd6df1eb7
Manikandan Nagarajan <manikandan.n2010@gmail.com>
2016-04-17 17:54:37 -0400

    Client changes for follow up questions

--- src/constants/Service.js
+++ src/constants/Service.js
@@ -1,1 +1,1 @@
-export const DEV_API_ROOT = 'http://ec2-54-191-212-102.us-west-2.compute.amazonaws.com:3000/';
+export const DEV_API_ROOT = 'http://ec2-54-187-4-228.us-west-2.compute.amazonaws.com:3000/';
